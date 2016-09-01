708ef5745e4154ee18ee1f23e080e8e20b0db731
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-09-01 20:48:24 -0300

    parameters readed

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ const router = express.Router();
 
 /* GET home page. */
 
+
+
 router.get('/', (req, res) =>{
     var fullUrl = req.get('host') + req.originalUrl;
   res.render('index', { title: ' API Basejump: Timestamp microservice', curPath: fullUrl });
 });
 
-
+router.get('/:date', (req, res) =>{
+    const date = req.params.date
+    console.log(date);
+  //res.render('index', { title: ' API Basejump: Timestamp microservice', curPath: fullUrl });
+});
 
 module.exports = router;
